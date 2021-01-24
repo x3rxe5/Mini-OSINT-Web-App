1afb1e852a5dd1633d00a55bfd9a3bcfca97129f
xerxes <xerxes@pop-os.localdomain>
2021-01-24 22:27:00 +0530

    Added Readme File

---
+++ README.md
@@ -0,0 +1,21 @@
+# Mini Hacking tools
+### Tools that you can integrate into your Web application
+
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Installing
+
+#### first clone the repo
+```
+git clone https://github.com/x3rxe5/Mini-OSINT-Web-App.git
+```
+
+#### install dependencies using pip
+```
+pip install -r requirements.txt
+```
+
+
--- server.py
+++ server.py
@@ -46,15 +46,63 @@ class Parser(Resource):
         if flag:
             self.statusCode =  200
 
+        return {
+            "data":res,
+            "statusCode":self.statusCode
+        }
+
+class HostDiscover(Resource):
+    def __init__(self):
+        self.statusCode = statusCode
+
+    def get(self):
+        res =  ""
+        flag = False
+        try:
+            command = ["python3","scripts/host_discover.py"]
+            res =  check_output(command,stderr=STDOUT).decode()
+            print("This is result -> "+res)
+            flag = True
+        except Exception as err:
+            res =  str(err)
+
+        if flag:
+            self.statusCode =  200
+        return {
+            "statusCode":self.statusCode,
+            "data":res
+        }
+
+    def post(self):
+        req = request.json
+        flag = False
+        url = req["url"]
+        res =  ""
+        command = req["command"]
+        try:
+            ter_command = ["python3","scripts/host_discover.py",url,command]
+            res =  check_output(ter_command,stderr=STDOUT).decode()
+            flag = True
+        except Exception as err:
+            res =  str(err)
+
+        if flag:
+            self.statusCode =  200
 
         return {
             "data":res,
             "statusCode":self.statusCode
         }
 
+
 # Route for Network Identification Object
 parser = Parser(404)
+host_discover = HostDiscover(404)
 api.add_resource(Parser,"/parser",resource_class_kwargs={'statusCode': parser})
+api.add_resource(HostDiscover,"/hostdiscover",resource_class_kwargs={'statusCode': host_discover})
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
